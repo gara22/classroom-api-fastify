79695abf2a35ac8cfd314733072dd9bfa87122ae
Gergely Garai <garai.gergely22@gmail.com>
2023-04-29 16:26:47 +0200

    added prisma

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fastify_1 = __importDefault(require("fastify"));
-const server = (0, fastify_1.default)();
-server.get('/ping', async (request, reply) => {
+const cors_1 = __importDefault(require("@fastify/cors"));
+const client_1 = require("@prisma/client");
+const app = (0, fastify_1.default)();
+const prisma = new client_1.PrismaClient();
+app.register(cors_1.default, {
+    origin: "http://localhost:5173",
+    methods: ["POST", "GET"]
+});
+app.get('/ping', async (request, reply) => {
     return 'pong\n';
 });
-server.listen({ port: 8080 }, (err, address) => {
+app.get('/classrooms', async (req, res) => {
+    const classrooms = await prisma.classroom.findMany();
+    return classrooms;
+});
+app.listen({ port: 8080 }, (err, address) => {
     if (err) {
         console.error(err);
         process.exit(1);
--- index.ts
+++ index.ts
@@ -1,12 +1,26 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
 
-const server = fastify()
 
-server.get('/ping', async (request, reply) => {
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.register(cors, {
+  origin: "http://localhost:5173",
+  methods: ["POST", "GET"]
+})
+
+app.get('/ping', async (request, reply) => {
   return 'pong\n'
 })
 
-server.listen({ port: 8080 }, (err, address) => {
+app.get('/classrooms', async (req, res) => {
+  const classrooms = await prisma.classroom.findMany();
+  return classrooms;
+})
+
+app.listen({ port: 8080 }, (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
